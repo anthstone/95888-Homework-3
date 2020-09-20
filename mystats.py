bf16c5ff205862a71a656f46fcb904b4d2a2936f
anthstone <71052305+anthstone@users.noreply.github.com>
2020-09-20 15:38:18 -0400

    expenses text

---
+++ mystats.py
@@ -0,0 +1,42 @@
+
+# File: mystats.py
+
+# define the mean function here
+
+# define the stddev function here
+
+# define the median function here
+
+# define the mode function here
+
+# part (a)
+print('The current module is:', __name__)
+
+# part (b)
+# print('mean(1) should be 1.0, and is:', mean(1))
+# print('mean(1,2,3,4) should be 2.5, and is:',
+#                                     mean(1,2,3,4))
+# print('mean(2.4,3.1) should be 2.75, and is:',
+#                                     mean(2.4,3.1))
+# print('mean() should FAIL:', mean())
+
+# part (c)
+# print('mean([1,1,1,2]) should be 1.25, and is:',
+#                                mean([1,1,1,2]))
+# print('mean((1,), 2, 3, [4,6]) should be 3.2,' +
+#       'and is:', mean((1,), 2, 3, [4,6]))
+
+# part (d)
+# your code here
+
+# part (e)
+# your code here
+
+# part (f)
+# your code here
+
+# part (g)
+# your code here
+
+# part (h)
+# your code here
