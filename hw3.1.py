1f396c6b0e6e845435cd8612d9ff98635a75a799
anthstone <71052305+anthstone@users.noreply.github.com>
2020-09-21 21:54:08 -0400

    Update hw3.1.py

--- hw3.1.py
+++ hw3.1.py
@@ -44,5 +44,27 @@ for rn in range(len(rec_num_to_record)):
 
 for i in rec_num_to_record.items():
 		    print('{:3d}: {}'.format(i[0], i[1]))
+            
+            
+#2). Variadic Functions
+"""
+You will need to modify the file mystats.py for this part of the homework, 
+since we will use mystats.py as a module to import in part 3 of the homework.  
+Your mystats.py file must be included in the .zip archive that you upload when 
+you have completed this homework.
+
+HINT: It is cheating to implement your mean function by calling the mean 
+function from some other library/module.  Write your own code!  Check 
+Wikipedia, Wolfram, or elsewhere to make sure you have the correct formulas.
+"""
+
+#3A). 
+"""
+a.	Open the mystats.py file (uploaded to Canvas as part of this homework) for editing and running with Python.  You will see that mystats.py mostly consists of comments describing functions that you will need to write, with a single uncommented print() function call that displays the name of the module itself.
+
+print('The current module is:', __name__)
+
+Run the module, and add a comment after the print() function call describing the output when mystats.py is the main module, that is, the module that you are currently editing and testing.
+
 
 
